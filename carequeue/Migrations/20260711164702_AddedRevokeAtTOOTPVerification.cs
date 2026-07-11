20f0349f67ea50985806cecd44d9bab55cbd2a42
Aryan Rajeev <raghavaan27@gmail.com>
2026-07-11 22:23:25 +0530

    Updated DB with updated OTP Entity

---
+++ carequeue/Migrations/20260711164702_AddedRevokeAtTOOTPVerification.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace carequeue.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRevokeAtTOOTPVerification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RevokedAt",
+                table: "OtpVerifications",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RevokedAt",
+                table: "OtpVerifications");
+        }
+    }
+}
